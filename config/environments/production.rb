74227b70cd67134d703c7c96acff1f04aff479a5
yoshuavic <yoshuavic@gmail.com>
2018-02-02 23:35:41 +0700

    try smtp setting 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com"
+    address: "smtp.gmail.com",
     port: 587,
     domain: "registration-firebiz.herokuapp.com",
     authentication: "plain",
