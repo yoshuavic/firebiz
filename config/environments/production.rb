5638100fb1f915a5c0ef1d7700e070f976dd4456
yoshuavic <yoshuavic@gmail.com>
2018-02-02 23:44:54 +0700

    try smtp setting 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,16 +22,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "registration-firebiz.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "yoshuavic85@gmail.com",
-    password: "meet424fact524"
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain "registration-firebiz.herokuapp.com",
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :user_name => "yoshuavic85@gmail.com",
+    :password => "meet424fact524"
   }
 
   # Full error reports are disabled and caching is turned on.
